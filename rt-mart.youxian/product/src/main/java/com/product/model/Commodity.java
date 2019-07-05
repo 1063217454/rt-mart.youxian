885e0478279ff5cb546785166ddaafc4f8e1415a
fp <1063217454@qq.com>
2019-07-06 00:27:50 +0800

    新建commodity表相关，【首页商品信息列表】

---
+++ rt-mart.youxian/product/src/main/java/com/product/model/Commodity.java
@@ -0,0 +1,34 @@
+package com.product.model;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+
+/**
+ * 便于管理首页商品信息列表而建
+ */
+@Data
+public class Commodity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)//主键自增
+    private Integer commodityId;//首页商品信息列表ID
+
+    private Integer product_id;//商品ID
+
+    private String categoryName;//分类名称
+
+    private String categoryCode;//分类编码
+
+    private Integer parentId;//父分类ID
+
+    private Integer categoryLevel;//分类层级
+
+    private Integer categoryStatus;//分类状态
+
+    private Date modifiedTime;//最后修改时间
+
+}
---
+++ rt-mart.youxian/product/src/main/java/com/product/repository/CommodityRepository.java
@@ -0,0 +1,10 @@
+package com.product.repository;
+
+import com.product.model.Commodity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommodityRepository extends JpaRepository<Commodity,Integer> {
+    List<Commodity> findByCategoryLevel(Integer categoryLevel);
+}
